6d9b90a417d440d128a95cf08313b47d9a2867f3
ZPLOVEXYH <1031626336@qq.com>
2017-05-17 14:13:53 +0800

    测试

--- src/main/java/com/spring/boot/Springboot03Application.java
+++ src/main/java/com/spring/boot/Springboot03Application.java
@@ -2,11 +2,19 @@ package com.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Springboot03Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot03Application.class, args);
+	}
+
+	@RequestMapping("/test")
+	public String get(){
+		return "hello world!!!";
 	}
 }
